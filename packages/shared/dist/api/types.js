24ee7e6ddc4c5bf9096cf3e9e6ff96ed11e5fb75
shoyosb-maker <shoyosb@gmail.com>
2026-06-05 05:39:13 -0500

    Delete packages directory

--- packages/shared/dist/api/types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * Tipos compartidos para toda la aplicación
- * Estos tipos los usan tanto backend como frontend
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=types.js.map
